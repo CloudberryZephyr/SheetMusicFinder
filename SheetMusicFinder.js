77ab8169a5bfe9c42c95be0e6d08863b58735266
CloudberryZephyr <ShoemakerCM21@gcc.edu>
2024-05-22 18:21:18 -0400

    untested getResponse()

--- SheetMusicFinder.js
+++ SheetMusicFinder.js
@@ -1,7 +1,31 @@
-const chunks = [];
+let chunks = [];
+let bodyData = '';
 
-function main() {
-	
+mediaRecorder.ondataavailable = (e) => {chunks.push(e.data);}
+
+async function startRecord(stream) {
+	const mediaRecorder = new MediaRecorder(stream);
+	mediaRecorder.start();
+	await setTimeout(mediaRecorder.stop(), 5000); // this should get us 5 seconds of sound data
+}
+
+function getMicInput(stream) {
+	startRecord(stream);
+	let chunkString = chunks.toString;
+	let base64Str = btoa(chunkString);
+	return base64Str;
+} 
+
+async function setUpMic() {
+	// make sure that getUserMedia is supported in the browser
+	if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {  
+		navigator.mediaDevices.getUserMedia({audio : true})
+			.then( (stream) => { bodyData = getMicInput(stream);})
+			.catch( (err) => {console.error(`getUserMedia  error: ${err}`);} );
+		
+	} else {
+		console.log("getUserMedia not supported on this browser");
+	}
 }
 
 async function getResponse() {
@@ -13,7 +37,7 @@ async function getResponse() {
 			'X-RapidAPI-Key': '0bfb0321bbmsh8e25be16e31863dp15994cjsnc481a9a41b94',
 			'X-RapidAPI-Host': 'shazam.p.rapidapi.com'
 		},
-		body: ''
+		body: bodyData
 	};
 
 	try {
@@ -25,26 +49,3 @@ async function getResponse() {
 		console.error(error);
 	}
 }
-
-async function setUpMic() {
-	// make sure that getUserMedia is supported in the browser
-	if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {  
-		navigator.mediaDevices.getUserMedia({audio : true})
-			.then( (stream) => getMicInput(stream) )
-			.catch( (err) => {console.error(`getUserMedia  error: ${err}`);} );
-		
-	} else {
-		console.log("getUserMedia not supported on this browser")
-	}
-}
-
-function getMicInput(stream) {
-	const mediaRecorder = new MediaRecorder(stream);
-	mediaRecorder.start();
-
-}
-
-mediaRecorder.ondataavailable = (e) => {chunks.push(e.data);}
- 
-
-main();
