74380b58443799fe2d7eadad4ccbd3f7cae4f82c
CloudberryZephyr <ShoemakerCM21@gcc.edu>
2024-06-03 12:58:09 -0400

    shelving this, practicing baby steps

--- SheetMusicFinder.js
+++ SheetMusicFinder.js
@@ -1,13 +1,47 @@
-let mediaRecorder;
 let chunks = [];
 let base64Str = "";
+let recorder;
+
+async function setUpAudio() {
+	navigator.getUserMedia = navigator.getUserMedia || 
+                         navigator.webkitGetUserMedia || 
+                         navigator.mozGetUserMedia || 
+                         navigator.msGetUserMedia;
+ 
+	let audioStream;
+ 
+	if (navigator.getUserMedia) {
+   		await navigator.getUserMedia({audio: true}, function(stream){
+    		audioStream = stream;
+		}, function(error){
+    		alert('Error capturing audio.');
+		});
+	}
+	else {
+	   	alert('getUserMedia not supported in this browser.');
+	}
+	
+	// creates the an instance of audioContext
+	const context = window.AudioContext;
+	const audioContext = new context();
+ 
+	// get processor module
+	await audioContext.audioWorklet.addModule("./linear-pcm-processor.js");
+	recorder = new AudioWorkletNode(audioContext, "linear-pcm-processor");
+                                                           
+	// we connect the recorder
+	audioContext.connect(recorder);
+
+	recorder.port.onmessage = (e) => {chunks.push(e.data)};
+}
+
 
 async function getResponse() {
 	base64Str = "";
-	mediaRecorder.start();
+	recorder.connect(audioContext.destination);
 	new Promise( function (resolve, reject) {
 		setTimeout( function() {
-			mediaRecorder.stop();
+			recorder.disconnect();
 			let chunkString = chunks.toString;
 			chunks = [];
 			base64Str = btoa(chunkString);
@@ -38,27 +72,33 @@ async function getResponse() {
 	});
 }
 
-document.addEventListener("DOMContentLoaded", () => {
-	// make sure that getUserMedia is supported in the browser
-	if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {  
-		navigator.mediaDevices.getUserMedia({audio : true})
-			.then( async function(stream) { 
-				// set up mediaRecorder
-				mediaRecorder = new MediaRecorder(stream);
+document.addEventListener("DOMContentLoaded", async () => {
+	setUpAudio();
+
+	// set up button trigger
+	const btn = document.getElementById("mic-button");
+	btn.addEventListener("click", getResponse)
+})
+
+// document.addEventListener("DOMContentLoaded", () => {
+// 	// make sure that getUserMedia is supported in the browser
+// 	if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {  
+// 		navigator.mediaDevices.getUserMedia({audio : true})
+// 			.then( async function(stream) { 
+// 				// set up mediaRecorder
+// 				mediaRecorder = new MediaRecorder(stream);
 				
-				// mediaRecorder.ondataavailable = (e) => {chunks.push(e.data)}
-				mediaRecorder.addEventListener("dataavailable", (e) => {chunks.push(e.data)} );
+// 				// mediaRecorder.ondataavailable = (e) => {chunks.push(e.data)}
+// 				mediaRecorder.addEventListener("dataavailable", (e) => {chunks.push(e.data)} );
 
-				// set up button trigger
-				const btn = document.getElementById("mic-button");
-				btn.addEventListener("click", getResponse)
-			})
-			.catch( (err) => {console.error(`getUserMedia error: ${err}`);} );
+// 				// set up button trigger
+// 				const btn = document.getElementById("mic-button");
+// 				btn.addEventListener("click", getResponse)
+// 			})
+// 			.catch( (err) => {console.error(`getUserMedia error: ${err}`);} );
 	
-	} else {
-		console.log("getUserMedia not supported on this browser");
-	}
+// 	} else {
+// 		console.log("getUserMedia not supported on this browser");
+// 	}
 
-});
-
-
+// });
