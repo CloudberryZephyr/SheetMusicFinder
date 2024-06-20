8d92fc2d38c85ba49a07c97c4575b6617218b8a4
CloudberryZephyr <ShoemakerCM21@gcc.edu>
2024-06-20 16:45:02 -0400

    Ready for github pages

--- SheetMusicFinder.js
+++ SheetMusicFinder.js
@@ -2,6 +2,7 @@ let recorder;
 let audioContext;
 let leftChannel;
 let recordingLength;
+let searchTerm = "";
 
 const bufferSize = 2048;
 
@@ -16,11 +17,11 @@ function mergeBuffers(channelBuffer, recordingLength) {
 	}
   
 	return Array.prototype.slice.call(result);
- }
+}
 
 
 
- function getAudioData() {
+function getAudioData() {
 	return new Promise( function (resolve, reject) {
 		setTimeout( function() {
 			recorder.disconnect();
@@ -43,11 +44,13 @@ function mergeBuffers(channelBuffer, recordingLength) {
 
 			// convert audio to string for http api request
 			let base64Str = btoa(charArr.join(""));
+
+			// resolve promise
 			resolve(base64Str)
 		}, 4000);
 
 	})
- }
+}
 
 
 
@@ -108,7 +111,7 @@ async function getResponse() {
 
 	getAudioData().then( async function (base64Str) {
 		// set up API call
-		const url = 'https://shazam.p.rapidapi.com/songs/detect';
+		const url = 'https://shazam.p.rapidapi.com/songs/v2/detect';
 		const options = {
 			method: 'POST',
 			headers: {
@@ -124,17 +127,81 @@ async function getResponse() {
 			const result = await response.text();
 		
 			console.log(result);
+
+			const searchLabel = document.getElementById("p1");
+
+			if (result.includes("\"matches\":[]")) {
+				searchLabel.textContent = "Audio not recognized.  Please retry."
+				searchTerm = "";
+			} else {
+				const artist = result.split("trackartist}\":")[1].split("\"")[1];
+				const title = result.split("\"title\":")[1].split("\"")[1];
+
+				searchTerm = title + " " + artist;
+				searchLabel.textContent = "Search by " + title + " by " + artist;
+			}
 		} catch (error) {
 			console.error(error);
 		}
 	});
 }
 
-document.addEventListener("DOMContentLoaded", () => {
-	// set up button trigger
-	const btn = document.getElementById("mic-button");
-	btn.addEventListener("click", getResponse)
+function redirect() {
+	// https://musescore.com/sheetmusic?text=caribbean%20blue%20enya
+	// default https://musescore.com/
 
+	if (searchTerm == "") {
+		window.location.href = "https://musescore.com/";
+	} else {
+		const formattedSearchTerm = "https://musescore.com/sheetmusic?text=" + searchTerm.replace(new RegExp(" ", "g"), "%20");
+		console.log(formattedSearchTerm);
+		window.location.href = formattedSearchTerm;
+	}
+
+}
+
+
+
+// for testing with canned data
+function testCanned() {
+	fetch("gooddata.raw")
+	.then((data) => res.text())
+	.then( async (data) => {
+		// do something with "text"
+		const url = 'https://shazam.p.rapidapi.com/songs/v2/detect';
+		const options = {
+			method: 'POST',
+			headers: {
+				'content-type': 'text/plain',
+				'X-RapidAPI-Key': '0bfb0321bbmsh8e25be16e31863dp15994cjsnc481a9a41b94',
+				'X-RapidAPI-Host': 'shazam.p.rapidapi.com'
+			},
+			body: data
+		};
+
+		try {
+			const response = await fetch(url, options);
+			const result = await response.text();
+		
+			console.log(result);
+		} catch (error) {
+			console.error(error);
+		}
+	})
+	.catch((e) => console.error(e));
+}
+
+
+
+document.addEventListener("DOMContentLoaded", () => {
+	// set up audio sample trigger
+	const sensebtn = document.getElementById("mic-button");
+	//sensebtn.addEventListener("click", getResponse);
+	sensebtn.addEventListener("click", testCanned)
+
+	// set up musecore search button
+	const searchbtn = document.getElementById("search-button");
+	searchbtn.addEventListener("click", redirect)
 });
 
 
