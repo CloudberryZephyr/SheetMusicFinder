fcc05f9bd5f6f0aef01b93ae83fb66382c5c4776
CloudberryZephyr <ShoemakerCM21@gcc.edu>
2024-07-01 14:41:31 -0400

    AudioWorklet - test transformation processor-side

--- linear-pcm-processor.js
+++ linear-pcm-processor.js
@@ -10,26 +10,26 @@ class LinearPCMProcessor extends AudioWorkletProcessor {
      */
     process(inputList, _outputList, _parameters) {
 
-        // let buffer = [];
+        let buffer = [];
 
         const input = inputList[0][0]; // first channel of first input
 
         
-        // for (let i = 0; i < input.length; i++) {
-        //     // convert from pcm 32 to pcm 16
-        //     let val = Math.floor(32767 * input[i]);
-        //     val = Math.min(32767, val);
-        //     val = Math.max(-32768, val);
+        for (let i = 0; i < input.length; i++) {
+            // convert from pcm 32 to pcm 16
+            let val = Math.floor(32767 * input[i]);
+            val = Math.min(32767, val);
+            val = Math.max(-32768, val);
 
-        //     // transform to ASCII char
-        //     let low = val & 255;
-        //     let high = (val & (255 << 8)) >> 8;
+            // transform to ASCII char
+            let low = val & 255;
+            let high = (val & (255 << 8)) >> 8;
 
-        //     buffer.push(String.fromCharCode(low));
-        //     buffer.push(String.fromCharCode(high));
-        // }
+            buffer.push(String.fromCharCode(low));
+            buffer.push(String.fromCharCode(high));
+        }
  
-        this.port.postMessage(input);
+        this.port.postMessage(buffer);
         return true;
     }
   }
