bfb0931538036da79a5423b67182ab267b6f8b99
CloudberryZephyr <117675972+CloudberryZephyr@users.noreply.github.com>
2024-07-03 11:09:54 -0400

    Testing 0

--- SheetMusicFInderTest.js
+++ SheetMusicFInderTest.js
@@ -3,14 +3,13 @@ let audioContext;
 let leftChannel;
 let recordingLength;
 let searchTerm = "";
-// let chunks = [];
 
 const bufferSize = 2048;
 
 function mergeBuffers(channelBuffer, recordingLength) {
 	let result = new Float32Array(recordingLength);
 	let offset = 0;
-
+  
 	for (let i = 0; i < channelBuffer.length; i++)
 	{
 		result.set(channelBuffer[i], offset);
@@ -40,8 +39,10 @@ function getAudioData() {
 				charArr.push(String.fromCharCode(low));
 				charArr.push(String.fromCharCode(high));
 			}
+
 			// convert audio to string for http api request
 			let base64Str = btoa(charArr.join(""));
+
 			// resolve promise
 			resolve(base64Str)
 		}, 4000);
@@ -59,7 +60,6 @@ async function getResponse() {
 		if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {  
 			await navigator.mediaDevices.getUserMedia({audio : true})
 				.then( async function(stream) {
-
 					let audioStream = stream;
 
 					// creates the an instance of audioContext
@@ -71,16 +71,15 @@ async function getResponse() {
 
 					// creates an audio node from the microphone incoming stream
 					const audioInput = audioContext.createMediaStreamSource(audioStream);
-
-					// connect the stream to the gain node
+					
+                    // connect the stream to the gain node
 					audioInput.connect(volume);
-
 					recorder = audioContext.createScriptProcessor.call(audioContext, bufferSize, 1, 1);
-
-					// we connect the recorder
+					
+                    // we connect the recorder
 					volume.connect(recorder);
-
-					recorder.onaudioprocess = function(event){
+					
+                    recorder.onaudioprocess = function(event){
 						const samples = event.inputBuffer.getChannelData(0);
 				
 						// we clone the samples
@@ -97,7 +96,6 @@ async function getResponse() {
 			console.log("getUserMedia not supported on this browser");
 		}
 	}
-
 	// begin recording
 	recorder.connect(audioContext.destination);
 
@@ -119,46 +117,25 @@ async function getResponse() {
 			const result = await response.text();
 		
 			console.log(result);
-
-
 			const searchLabel = document.getElementById("p1");
-
 			if (result.includes("\"matches\":[]")) {
 				searchLabel.textContent = "Audio not recognized.  Please retry."
 				searchTerm = "";
 			} else {
 				const artist = result.split("trackartist}\":")[1].split("\"")[1];
 				const title = result.split("\"title\":")[1].split("\"")[1];
-				
 				searchTerm = title + " " + artist;
 				searchLabel.textContent = "Search by " + title + " by " + artist;
 			}
-
 		} catch (error) {
 			console.error(error);
 		}
 	});
 }
 
-					// // get processor module
-					// await audioContext.audioWorklet.addModule("./linear-pcm-processor.js");
-					// recorder = new AudioWorkletNode(audioContext, "linear-pcm-processor");
-
-					// // we connect the recorder
-					// volume.connect(recorder);
-					
-					// // this will push the data returned from linear-pcm-processor process()
-					// // this data is an array of ASCII chars
-					// recorder.port.onmessage = (e) => {
-					// 	const samples = new Float32Array(e.data);
-					// 	chunks.push(samples); 
-					// 	recordingLength += samples.length;
-					// }
-
 function redirect() {
 	// https://musescore.com/sheetmusic?text=caribbean%20blue%20enya
 	// default https://musescore.com/
-
 	if (searchTerm == "") {
 		window.location.href = "https://musescore.com/";
 	} else {
@@ -166,36 +143,13 @@ function redirect() {
 		console.log(formattedSearchTerm);
 		window.location.href = formattedSearchTerm;
 	}
-
 }
-
-
 
 // for testing with canned data
 function testCanned() {
-	fetch("badData.txt")
+	fetch("baddata.raw")
 	.then((result) => result.text())
 	.then( async (data) => {
-		let dataArr = data.split(",\n");
-
-		let arr = [];
-
-		// format audio to pcm signed integer 16bit mono
-		for (let i = 0; i < dataArr.length; i++) {
-			let val = Math.floor(32767 * parseFloat(dataArr[i]));
-			val = Math.min(32767, val);
-			val = Math.max(-32768, val);
-
-			let low = val & 255;
-			let high = (val & (255 << 8)) >> 8;
-
-			arr.push(String.fromCharCode(low));
-			arr.push(String.fromCharCode(high));
-		}
-
-		// convert audio to string for http api request
-		let base64Str = btoa(arr.join(""));
-
 		// do something with "text"
 		const url = 'https://shazam.p.rapidapi.com/songs/v2/detect';
 		const options = {
@@ -205,9 +159,8 @@ function testCanned() {
 				'X-RapidAPI-Key': '0bfb0321bbmsh8e25be16e31863dp15994cjsnc481a9a41b94',
 				'X-RapidAPI-Host': 'shazam.p.rapidapi.com'
 			},
-			body: base64Str
+			body: data
 		};
-
 		try {
 			const response = await fetch(url, options);
 			const result = await response.text();
@@ -220,17 +173,13 @@ function testCanned() {
 	.catch((e) => console.error(e));
 }
 
-
-
 document.addEventListener("DOMContentLoaded", () => {
 	// set up audio sample trigger
 	const sensebtn = document.getElementById("mic-button");
-	sensebtn.addEventListener("click", getResponse);
-	//sensebtn.addEventListener("click", testCanned);
+	//sensebtn.addEventListener("click", getResponse)
+	sensebtn.addEventListener("click", testCanned)
 
 	// set up musecore search button
 	const searchbtn = document.getElementById("search-button");
 	searchbtn.addEventListener("click", redirect)
 });
-
-
